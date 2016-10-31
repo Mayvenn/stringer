edde28ce69358053d4a3fe20364b8db48322cdca
Corey Reece <corey@mayvenn.com>
2016-10-31 11:48:54 -0700

    Firmed up API
    Captured device
    Public commands ns/interface
    Use Object.assign to merge objects

--- stringer.js
+++ stringer.js
@@ -1,50 +1,74 @@
-(function (global, undefined) {
+(function (window, undefined) {
   var self = {
     serverURI: "http://localhost:8080",
+    device: {},
     queue: [],
-    debug: false
+    debug: true
   };
+
+  var publicCommands = {};
 
   self.load = function () {
-    if (global.hasOwnProperty("stringer") &&
-        Array.isArray(global.stringer)) {
-      log("load", global.stringer);
-      global.stringer.forEach(function(e) {
+    if (window.hasOwnProperty("stringer") &&
+        Array.isArray(window.stringer)) {
+      log("load", window.stringer);
+
+      window.stringer.forEach(function(e) {
         self.queue.push(e);
       }, self);
+
+      var rng = initRandom(window);
+
+      self.push = function() {
+        for (var i in arguments) {
+          var event = arguments[i];
+          var commandName = event[0];
+          var args = event[1];
+          args.uuid = uuid(rng);
+          args.ts = Date.now();
+          var cmd = publicCommands[commandName];
+          if (cmd) {
+            log("invoke", commandName, args);
+            cmd.call(null, args);
+          } else {
+            log("invalid command", commandName);
+          }
+        }
+        return arguments.length;
+      };
+
+      self.device = captureDevice();
+
+      self.push.apply(self, self.queue);
+      delete self.queue;
     }
 
     return self;
   };
 
-  self.init = function (config = {}) {
-    var rng = getRng(global);
+  publicCommands.config = function (config = {}) {
+    Object.assign(self, config);
+  };
 
-    for (var k in self) {
-      if (self.hasOwnProperty(k) &&
-          "function" != typeof self[k]) {
-        self[k] = config[k] || self[k];
-      }
+  publicCommands.track = function (args) {
+    if (args["eventName"]) {
+      send(Object.assign({},
+                         args,
+                         self.device,
+                         {pageURI: window.location.href,
+                          pageTitle: window.document.title,
+                          referrer: window.document.referrer}));
     }
+  };
 
-    log("init", self);
-
-    self.push = function() {
-      for (var i in arguments) {
-        var event = arguments[i];
-        log("push", event, self.serverURI);
-        event.uuid = uuid(rng);
-        event.ts = Date.now();
-        send(event);
-      }
-      return arguments.length;
+  function captureDevice() {
+    return {
+      userAgent: window.navigator.userAgent,
+      screenHeight: window.screen.height,
+      screenWidth: window.screen.width,
+      screenPixelRatio: window.devicePixelRatio
     };
-
-    self.push.apply(self, self.queue);
-    delete self.queue;
-
-    return self;
-  };
+  }
 
   function log() {
     if (self.debug &&
@@ -54,10 +78,17 @@
     }
   }
 
-  function getRng(global) {
-    // Allow for MSIE11 msCrypto
+  function send(payload) {
+    log("send", self.serverURI, payload);
+    xhr = new XMLHttpRequest();
+    xhr.open("POST", self.serverURI);
+    xhr.setRequestHeader("Content-Type", "text/plain");
+    xhr.send(JSON.stringify(payload));
+  }
+
+  function initRandom(window) {
     var _rng;
-    var _crypto = global.crypto || global.msCrypto;
+    var _crypto = window.crypto || window.msCrypto;
 
     if (!_rng && _crypto && _crypto.getRandomValues) {
       try {
@@ -83,15 +114,11 @@
     return _rng;
   }
 
-  function send(payload) {
-    log("send", self.serverURI, payload);
-    xhr = new XMLHttpRequest();
-    xhr.open("POST", self.serverURI);
-    xhr.setRequestHeader("Content-Type", "text/plain");
-    xhr.send(JSON.stringify(payload));
-  };
+  function uuid(rng) {
+    var rnds = rng();
+    rnds[6] = (rnds[6] & 0x0f) | 0x40;
+    rnds[8] = (rnds[8] & 0x3f) | 0x80;
 
-  function unparse(buf) {
     var i = 0,
         bth = [];
 
@@ -99,27 +126,42 @@
       bth[j] = (j + 0x100).toString(16).substr(1);
     }
 
-    return  bth[buf[i++]] + bth[buf[i++]] +
-      bth[buf[i++]] + bth[buf[i++]] + '-' +
-      bth[buf[i++]] + bth[buf[i++]] + '-' +
-      bth[buf[i++]] + bth[buf[i++]] + '-' +
-      bth[buf[i++]] + bth[buf[i++]] + '-' +
-      bth[buf[i++]] + bth[buf[i++]] +
-      bth[buf[i++]] + bth[buf[i++]] +
-      bth[buf[i++]] + bth[buf[i++]];
+    return bth[rnds[i++]] + bth[rnds[i++]] +
+        bth[rnds[i++]] + bth[rnds[i++]] + '-' +
+        bth[rnds[i++]] + bth[rnds[i++]] + '-' +
+        bth[rnds[i++]] + bth[rnds[i++]] + '-' +
+        bth[rnds[i++]] + bth[rnds[i++]] + '-' +
+        bth[rnds[i++]] + bth[rnds[i++]] +
+        bth[rnds[i++]] + bth[rnds[i++]] +
+        bth[rnds[i++]] + bth[rnds[i++]];
   }
 
-  function uuid(rng) {
-    var i = 0;
+  // -- Polyfills --
 
-    options = {};
-    var rnds = rng();
+  if (typeof Object.assign != 'function') {
+    (function () {
+      Object.assign = function (target) {
+        'use strict';
+        // We must check against these specific cases.
+        if (target === undefined || target === null) {
+          throw new TypeError('Cannot convert undefined or null to object');
+        }
 
-    rnds[6] = (rnds[6] & 0x0f) | 0x40;
-    rnds[8] = (rnds[8] & 0x3f) | 0x80;
-
-    return unparse(rnds);
+        var output = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+          var source = arguments[index];
+          if (source !== undefined && source !== null) {
+            for (var nextKey in source) {
+              if (source.hasOwnProperty(nextKey)) {
+                output[nextKey] = source[nextKey];
+              }
+            }
+          }
+        }
+        return output;
+      };
+    })();
   }
 
-  global.stringer = self.load();
+  window.stringer = self.load();
 })(window);
