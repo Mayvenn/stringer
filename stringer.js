60b465e8cae001a6ee80687ea4a60791e5c5f840
Jacob Maine <jacob@mayvenn.com>
2016-11-10 18:00:05 -0800

    Revert "Use navigator.sendBeacon, if available"
    This reverts commit 8d4322fe1ed6748a0515d0be0af85f97f87ca7a7.
    
    In theory this commit was fine, and it appeared to be working in Chrome.
    But jasmine-ajax doesn't mock navigator.sendBeacon, so tests were
    failing. Need to fix tests and try again.

--- stringer.js
+++ stringer.js
@@ -7,8 +7,7 @@
       visitor = fetchVisitor(),
       sourceSite = "default",
       serverURI = "https://t.mayvenn.com/",
-      debug = false,
-      send;
+      debug = false;
 
   // Only public functions/vars should be on self, otherwise leave them in the closure!
 
@@ -153,19 +152,12 @@
     }
   }
 
-  if (window.navigator.sendBeacon) {
-    send = function send(payload) {
-      log("send", serverURI, payload);
-      window.navigator.sendBeacon(serverURI, JSON.stringify(payload));
-    };
-  } else {
-    send = function send(payload) {
-      log("send", serverURI, payload);
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", serverURI);
-      xhr.setRequestHeader("Content-Type", "text/plain");
-      xhr.send(JSON.stringify(payload));
-    };
+  function send(payload) {
+    log("send", serverURI, payload);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", serverURI);
+    xhr.setRequestHeader("Content-Type", "text/plain");
+    xhr.send(JSON.stringify(payload));
   }
 
   // from https://github.com/broofa/node-uuid
