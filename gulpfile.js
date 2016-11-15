74207812e0e252ec809a12596cbde769b517afac
Jacob Maine <jacob@mayvenn.com>
2016-11-14 21:27:00 -0800

    gzip built JS file

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,25 @@ var gulp = require('gulp');
 var del = require('del');
 var uglify = require('gulp-uglify');
 var sha = require('gulp-gitshasuffix');
+var gzip = require('gulp-gzip');
 
-gulp.task('clean-min-js', function () {
-  return del(['./target']);
+var BUILD_DIR = "./target";
+
+gulp.task('clean-build', function () {
+  return del([BUILD_DIR]);
 });
 
-
-gulp.task('minify-and-version', ['clean-min-js'], function () {
+gulp.task('build-js', ['clean-build'], function () {
   return gulp.src(['stringer.js'])
-    .pipe(uglify().on('error', function(e){
-      console.log(e);
-    }))
+    .pipe(uglify().on('error', function(e) { console.log(e); }))
     .pipe(sha({length: 7}))
-    .pipe(gulp.dest('target/'));
+    .pipe(gzip({ append: false }))
+    .pipe(gulp.dest(BUILD_DIR));
 });
 
 
-gulp.task('default', ['minify-and-version']);
+gulp.task('default', ['build-js']);
 
 gulp.task('watch', function(){
-  gulp.watch('stringer.js', ['minify-and-version']);
+  gulp.watch('stringer.js', ['build-js']);
 })
